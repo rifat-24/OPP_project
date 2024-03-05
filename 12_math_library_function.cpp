38ceb3efd4bcab8fa0a2535a725b15608998b6ee
Rifat_Islam <161961946+rifat-24@users.noreply.github.com>
2024-03-05 23:09:31 +0600

    Add files via upload

---
+++ 12_math_library_function.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int num1 = 25, num2 = 7;
+    int res1 = sqrt(num1);
+    int res2 = pow(num2 , 2);
+    cout << res1 << endl;
+    cout << res2 << endl;
+
+}
