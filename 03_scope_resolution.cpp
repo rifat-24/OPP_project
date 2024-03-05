0be72cd40ec746fd9ac21b7eb7a29b7ddea723a1
Rifat_Islam <161961946+rifat-24@users.noreply.github.com>
2024-03-05 19:08:18 +0600

    Add files via upload

---
+++ 03_scope_resolution.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int num = 15;
+int main()
+{
+    int num = 50;
+    cout << "Value of global x is " << :: num << endl;
+    cout << "Value of local x is " << num << endl;
+}
+
+
