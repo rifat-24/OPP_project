fa320027b986952a31f83edf8131e1697ee250c4
Rifat_Islam <161961946+rifat-24@users.noreply.github.com>
2024-03-05 23:09:09 +0600

    Add files via upload

---
+++ 11_function_overlodding.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void add(int a, int b)
+{
+  cout << "sum = " << (a + b) << endl;
+}
+void add(double a, double b)
+{
+    cout << endl << "sum = " << (a + b) << endl;
+}
+
+int main()
+{
+    add(10, 2);
+    add(5.3, 6.2);
+
+}
