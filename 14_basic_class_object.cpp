211031029e83a82bf9db580b98c6394d12cdae01
Rifat_Islam <161961946+rifat-24@users.noreply.github.com>
2024-03-06 15:12:56 +0600

    Add files via upload

---
+++ 14_basic_class_object.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+class student
+{
+public:
+    void name()
+    {
+        string s;
+        cin >> s;
+        cout << s << endl;
+    }
+};
+int main()
+{
+    student ob;
+    ob.name();
+}
